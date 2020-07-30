16da566cc3287731b1a0b8acdc98332986760527
StormValen <valentin.g.l@gmail.com>
2020-07-30 11:10:28 +0200

    v.2 TechTest - deleted unnecesary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UserListItemComponent } from './components/user-list-item/user-list-item.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { UserListComponent } from './components/user-list/user-list.component';
@@ -12,7 +11,6 @@ import { UserListComponent } from './components/user-list/user-list.component';
 @NgModule({
     declarations: [
         AppComponent,
-        UserListItemComponent,
         LoaderComponent,
         UserPageComponent,
         UserListComponent
